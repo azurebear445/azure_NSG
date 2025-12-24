f8936071271f267e05875c9c06ce2761a0a4b895
azurebear445 <awsatlnta@gmail.com>
2025-12-24 08:56:02 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,13 +7,3 @@ output "name" {
   value       = azurerm_network_security_group.this.name
   description = "(string) - The name of the NSG created."
 }
-
-output "resource_group_name" {
-  value       = azurerm_network_security_group.this.resource_group_name
-  description = "(string) - The resource group name where the NSG was created."
-}
-
-output "location" {
-  value       = azurerm_network_security_group.this.location
-  description = "(string) - The Azure location where the NSG was created."
-}
