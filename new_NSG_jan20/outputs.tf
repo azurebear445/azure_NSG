6172a2ad586f002636116292e32a3150052c1e32
azurebear445 <awsatlnta@gmail.com>
2026-01-27 11:28:35 -0500

    Add files via upload

--- new_NSG_jan20/outputs.tf
+++ new_NSG_jan20/outputs.tf
@@ -1,4 +1,4 @@
 output "id" {
-  description = "The ID of the Network Security Group."
   value       = azurerm_network_security_group.this.id
+  description = "(string) - The ID of the Network Security Group."
 }
